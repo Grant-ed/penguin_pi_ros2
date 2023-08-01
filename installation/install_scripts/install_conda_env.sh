cb5318bdbbfaf043964e0e1fc5f36995c8ae8705
Grant-ed <grantvanbreda@gmail.com>
2023-08-01 11:43:54 +1000

    fix conda env in setup scripts

--- installation/install_scripts/install_conda_env.sh
+++ installation/install_scripts/install_conda_env.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo ""
-echo "---Creating driverless env---"
+echo "---Creating penguinpi env---"
 echo ""
 sleep 3
-cd ~/QUTMS/QUTMS_Driverless
-mamba env create --name driverless_env --file installation/humble_py310_dev_env.yml
-conda activate driverless_env
+cd ~/PenguinPiRos
+mamba env create --name penguinpi_env --file installation/humble_py310_dev_env.yml
+conda activate penguinpi_env
 conda config --env --add channels conda-forge
 conda config --env --add channels robostack-staging
 mamba install -y --file installation/conda_requirements.txt
