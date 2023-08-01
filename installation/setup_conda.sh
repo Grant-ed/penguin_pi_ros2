10f24ab6f0665b0e1d738bb612185e013b188ac3
Grant-ed <grantvanbreda@gmail.com>
2023-08-01 11:18:20 +1000

    Setup scripts

---
+++ installation/setup_conda.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+cd ~
+
+## Pre-reqs
+echo ""
+echo "---Installing pre-reqs---"
+echo ""
+sleep 3
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y git python3-pip pre-commit mesa-utils
+
+## Make workspace
+mkdir ~/PenguinPiRos
+cd ~/PenguinPiRos
+# directory for people to drop their rosbags in
+mkdir bags/
+
+## Clone PenguinPi repo
+echo ""
+echo "---Cloning PenguinPi repo---"
+echo ""
+sleep 3
+git clone --recurse-submodules https://github.com/Grant-ed/penguin_pi_urdf.git
+
+## Download and install mambaforge
+source ~/PenguinPiRos/installation/install_scripts/install_mambaforge.sh
+
+## Create ros_env development environment
+source ~/PenguinPiRos/installation/install_scripts/install_conda_env.sh
+
+## Create an alias for ease
+echo "alias a='conda activate ros_env && source install/setup.bash'" >> ~/.bashrc
+
+echo ""
+echo "---Building packages---"
+echo ""
+sleep 3
+colcon build --symlink-install --packages-up-to qutms_cli_tools
+# once the cli tools are built, we can use them to build the rest of the workspace
+build_workspace
+
+## Wrap up
+echo "Thank you for installing, explore the rest of the wiki"
