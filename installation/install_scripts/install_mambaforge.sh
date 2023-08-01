e54f8960593c872f495ee3d415a7185d8c90c9a3
Grant-ed <grantvanbreda@gmail.com>
2023-08-01 11:28:26 +1000

    changed from mambaforge to miniforge

--- installation/install_scripts/install_mambaforge.sh
+++ installation/install_scripts/install_mambaforge.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo ""
-echo "---Installing mambaforge---"
+echo "---Installing miniforge3---"
 echo ""
 sleep 3
 cd ~
-wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh
-bash Mambaforge-$(uname)-$(uname -m).sh
-source ~/mambaforge/bin/activate
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh
+bash Miniforge3-$(uname)-$(uname -m).sh
+source ~/miniforge3/bin/activate
 conda config --set auto_activate_base false
 conda deactivate
-rm -rf Mambaforge-$(uname)-$(uname -m).sh
+rm -rf Miniforge3-$(uname)-$(uname -m).sh
