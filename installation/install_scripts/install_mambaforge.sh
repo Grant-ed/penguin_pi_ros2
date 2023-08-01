c33486a4d3c7d1e8b6e5e957d8012a920ad17771
Grant-ed <grantvanbreda@gmail.com>
2023-08-01 12:24:10 +1000

    fixed non-interactive miniforge install flag

--- installation/install_scripts/install_mambaforge.sh
+++ installation/install_scripts/install_mambaforge.sh
@@ -6,7 +6,7 @@ echo ""
 sleep 3
 cd ~
 wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh
-bash Miniforge3-$(uname)-$(uname -m).sh /S
+bash Miniforge3-$(uname)-$(uname -m).sh -b
 source ~/miniforge3/bin/activate
 conda init
 conda install mamba -c conda-forge
