712cc2af40ba051c648a6293c6613f872280ab34
Grant-ed <grantvanbreda@gmail.com>
2023-08-01 11:58:55 +1000

    setup scripts source fix

--- installation/install_scripts/install_mambaforge.sh
+++ installation/install_scripts/install_mambaforge.sh
@@ -8,6 +8,8 @@ cd ~
 wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh
 bash Miniforge3-$(uname)-$(uname -m).sh
 source ~/miniforge3/bin/activate
+conda init
+conda install mamba -c conda-forge
 conda config --set auto_activate_base false
 conda deactivate
 rm -rf Miniforge3-$(uname)-$(uname -m).sh
