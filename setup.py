40dffd2029c2ddcbbc9f08622c6d52227f88c24e
Grant <grantvanbreda@gmail.com>
2023-08-09 23:19:32 +1000

    replace cmakelists.txt (old ros) with setup.py (new ros)

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'penguin_pi_urdf'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Your Name',
+    author_email='grantvanbreda@gmail.com',
+    maintainer='Your Name',
+    maintainer_email='grantvanbreda@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    entry_points={
+        'console_scripts': [
+            'microcontroller_node = penguin_pi_urdf.microcontroller_node:main',  # Update with the correct path to your node's main function
+        ],
+    },
+)
