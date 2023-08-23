05abf70e79476e6dba21e8ce78e92dc1c45f63d4
Grant <grantvanbreda@gmail.com>
2023-08-24 07:10:24 +1000

    missed a condition

--- src/ppi_config/launch/display_ekf.launch.py
+++ src/ppi_config/launch/display_ekf.launch.py
@@ -24,14 +24,7 @@ def generate_launch_description():
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher',
-        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui')),
         parameters=[ekf_config_path]
-    )
-    joint_state_publisher_gui_node = launch_ros.actions.Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
     )
     rviz_node = launch_ros.actions.Node(
         package='rviz2',
