a717e67ba5984a23b7e20c73e373a252f2c4440a
Grant-ed <grantvanbreda@gmail.com>
2023-10-13 13:25:54 +1000

    replaced old diffdrive with humble diffdrive

---
+++ src/diffdrive_arduino/hardware/include/diffdrive_arduino/penguinpi_comms.h
@@ -0,0 +1,25 @@
+#ifndef DIFFDRIVE_ARDUINO_PENGUINPI_COMMS_H
+#define DIFFDRIVE_ARDUINO_PENGUINPI_COMMS_H
+
+#include <cstdint>
+#include <Python.h>
+
+class PenguinPiComms {
+public:
+    PenguinPiComms();
+    ~PenguinPiComms();
+
+    void init();
+    void close();
+    void set_velocity(int8_t left, int8_t right);
+    uint16_t* get_encoders();
+    uint16_t* set_velocity_get_encoders(int8_t left, int8_t right);
+    void stop_all();
+    void clear_data();
+
+private:
+    PyObject *pModule;
+    PyObject *pInstance;
+};
+
+#endif // DIFFDRIVE_ARDUINO_PENGUINPI_COMMS_H
